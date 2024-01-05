884486084949305705337b6e785b130bd3d8dcb3
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-05 22:12:24 +0500

    added some components

---
+++ form-validation/app/(components)/Tooltip/Tooltip.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { useEffect } from "react";
+
+export default function Tooltip() {
+    useEffect(() => {
+        const init = async () => {
+            const { Tooltip, initTE } = await import("tw-elements");
+            initTE({ Tooltip });
+        };
+        init();
+    }, []);
+    return (
+
+        <div className="mt-16 flex justify-center">
+            <p className="text-lg">
+                Hover the link to see the
+                <a
+                    href="#"
+                    className="text-primary pl-1 transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+                    data-te-toggle="tooltip"
+                    title="Hi! I'm tooltip"
+                >
+                    tooltip
+                </a>
+            </p>
+        </div>
+    )
+}
--- form-validation/app/page.tsx
+++ form-validation/app/page.tsx
@@ -1,7 +1,14 @@
+import dynamic from "next/dynamic";
+
+const DynamicComponent = dynamic(()=> import("./(components)/Tooltip/Tooltip"), {
+  ssr: false,
+});
+
 export default function Home() {
   return (
     <main>
-      <h1>MAin Page of Application</h1>
+      <h1>Main Page of Application</h1>
+      <DynamicComponent/>
     </main>
   )
 }
