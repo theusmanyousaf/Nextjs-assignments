7baa5f1bda47fcaba4123aae13adf53140652d22
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-21 23:03:56 +0500

    basics done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# Nextjs-assignments for PIAIC's batch-50 quarter 2nd." 
+<h1>Nextjs-assignments for PIAIC's batch-50 quarter 2nd.</h1> 
--- usmanyousaf/app/page.tsx
+++ usmanyousaf/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import Footer from "./(components)/Footer/Footer";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="flex justify-center align-center font-bold text-4xl mt-4">Hi!</h1>
-    </main>
+    // <main>
+    //   <h1 className="flex justify-center align-center font-bold text-4xl mt-4">Hi!</h1>
+    // </main>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+          <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            Projects
+            <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </a>
+          <a href="/contact" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            Contact Me
+          </a>
+        </div>
+        <div className="hidden sm:mt-0 sm:col-span-5 sm:flex p-10">
+          <Image src="/IMG_0288.png" alt="usman" width={1200} height={1200}></Image>
+        </div>
+      </div>
+      <Footer/>
+    </section>
   );
 }
