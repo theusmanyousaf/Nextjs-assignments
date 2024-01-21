0bf94af7a175a139d13a7abbbd1530159232ffca
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-01-21 09:37:00 +0500

    portfolio-first-commit

---
+++ usmanyousaf/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="flex justify-center align-center font-bold text-4xl mt-4">Hi!</h1>
+    </main>
+  );
+}
